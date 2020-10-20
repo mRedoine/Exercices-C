61fd5ef85ea7344861fc11c0b379c4cda67e1a87
mRedoine <redoinekr31@gmail.com>
2020-10-20 16:30:38 +0200

    Update exo1.c

--- Chapitre 7/exo1.c
+++ Chapitre 7/exo1.c
@@ -1,7 +1,6 @@
 /* Devise a function called min(x,y) that returns the smaller of two double values. Test
 the function with a simple driver. */
 #include <stdio.h>
-#include "header.h"
 double min(double firstValue, double secondValue)
 {
  return firstValue < secondValue ? firstValue : secondValue;
